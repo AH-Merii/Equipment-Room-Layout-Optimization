eb58e92b2bd70c9c65c1151103cce63772c54f02
AbdulHamid Merii <ah_merii@outlook.com>
2025-11-24 14:45:01 +0000

    Add building logic

--- building_simulator/building.py
+++ building_simulator/building.py
@@ -1,4 +1,20 @@
+from building_simulator.floor import Floor
+
+
 class Building:
     """
     Represents the building containing floors.
     """
+
+    def __init__(self, name: str):
+        self.name = name
+        self.floors: dict[int, Floor] = {}
+
+    def add_floor(self, floor: Floor):
+        self.floors[floor.floor_level] = floor
+
+    def get_floor(self, level: int) -> Floor | None:
+        return self.floors.get(level)
+
+    def __repr__(self):
+        return f"Building(Name: {self.name}, Floors: {list(self.floors.keys())})"
